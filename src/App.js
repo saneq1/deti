3b862c3b3850cab0ea29af5f9628fb53dc590fe6
saneq <thesaneq@gmail.com>
2020-03-05 10:43:16 +0300

    add: link and reboot style

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
+import './assets/base.scss'
 import './App.scss';
 import logo from './assets/img/logo.png'
 
 function App() {
   return (
       <div className="app">
-        <header className="app-header">
-          <div className={'app-header-top'}>
-
-              <div className={'app-header-bottom'}>
+        <header className="header">
+          <div className={'header-top'}>
+              <div className={'header-bottom'}>
                 <div className="container">
+                  <div className="row">
                 <img src={logo} alt="logo"/>
-                <span>усыновление в россии</span>
+                <h1>усыновление <br/> в россии</h1>
+                  </div>
               </div>
             </div>
           </div>
-
+          <nav>
+            <ul>
+              <li><a href="">About</a></li>
+              <li><a href="">Forum</a></li>
+              <li><a href="">Download</a></li>
+            </ul>
+          </nav>
         </header>
       </div>
   );
